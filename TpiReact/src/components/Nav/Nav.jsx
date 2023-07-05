962714d4c74e3bd39e501dd5628c08752542365f
Fausto Savoya <84111811+FaustoSav@users.noreply.github.com>
2023-07-05 13:22:09 -0300

    Update Nav.jsx

--- TpiReact/src/components/Nav/Nav.jsx
+++ TpiReact/src/components/Nav/Nav.jsx
@@ -49,7 +49,7 @@ const Nav = () => {
         </NavLink>
       </div>
 
-      <div className="md:text-lg xl:text-xl hover:underline" onClick={handleLogOut}>
+      <div className="md:text-lg xl:text-xl hover:underline cursor:pointer" onClick={handleLogOut}>
         Logout{" "}
       </div>
     </div>
